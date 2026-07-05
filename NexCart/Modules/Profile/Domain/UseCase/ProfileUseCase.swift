6729a29eded293c7dcbc00b1080c0b4ee7fa6903
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2026-07-05 11:31:32 -0700

    profile use case

--- NexCart/Modules/Profile/Domain/UseCase/ProfileUseCase.swift
+++ NexCart/Modules/Profile/Domain/UseCase/ProfileUseCase.swift
@@ -5,3 +5,20 @@
 //  Created by Antoneos Philip on 05/07/2026.
 //
 
+import Foundation
+
+protocol ProfileUseCaseProtocol {
+    func execute() async -> UserEntity?
+}
+
+class ProfileUseCase: ProfileUseCaseProtocol {
+    private let repo: ProfileRepoProtocol
+    
+    init(repo: ProfileRepoProtocol = ProfileRepo()) {
+        self.repo = repo
+    }
+    
+    func execute() async -> UserEntity? {
+        return await repo.getUserProfile()
+    }
+}
