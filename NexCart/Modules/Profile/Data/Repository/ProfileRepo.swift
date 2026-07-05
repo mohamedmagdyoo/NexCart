34eaebe5087bf5b7c47055e8d15ef42b6fbda57d
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2026-07-05 11:31:16 -0700

    profile repo impl

--- NexCart/Modules/Profile/Data/Repository/ProfileRepo.swift
+++ NexCart/Modules/Profile/Data/Repository/ProfileRepo.swift
@@ -4,3 +4,11 @@
 //
 //  Created by Antoneos Philip on 05/07/2026.
 //
+
+import Foundation
+
+class ProfileRepo: ProfileRepoProtocol {
+    func getUserProfile() async -> UserEntity? {
+        return await AppConstants.shared.getUserEntity()
+    }
+}
