24f5b0e4f920ab6008da24d0f4cb67dadf7f3849
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2026-07-06 19:20:43 -0700

    complete orders repo

--- NexCart/Modules/CompleatOrderFlow/CompleteOrder/Domain/CompleteOrderRepositoryProtocol.swift
+++ NexCart/Modules/CompleatOrderFlow/CompleteOrder/Domain/CompleteOrderRepositoryProtocol.swift
@@ -5,3 +5,8 @@
 //  Created by Antoneos Philip on 06/07/2026.
 //
 
+import Foundation
+
+protocol CompleteOrderRepositoryProtocol {
+    func placeOrder(orderInput: OrderCreateBody) async throws -> CompleteOrderEntity
+}
