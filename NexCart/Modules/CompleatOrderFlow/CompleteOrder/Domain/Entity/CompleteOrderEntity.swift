097b8d9818628642c31f2951a80b69a67ca23c69
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2026-07-06 19:21:15 -0700

    complete orders use case

--- NexCart/Modules/CompleatOrderFlow/CompleteOrder/Domain/Entity/CompleteOrderEntity.swift
+++ NexCart/Modules/CompleatOrderFlow/CompleteOrder/Domain/Entity/CompleteOrderEntity.swift
@@ -5,3 +5,11 @@
 //  Created by Antoneos Philip on 06/07/2026.
 //
 
+import Foundation
+
+struct CompleteOrderEntity {
+    let id: Int
+    let orderNumber: String
+    let email: String
+    let totalPrice: String
+}
--- NexCart/Modules/CompleatOrderFlow/CompleteOrder/Domain/UseCase/CompleteOrderUseCase.swift
+++ NexCart/Modules/CompleatOrderFlow/CompleteOrder/Domain/UseCase/CompleteOrderUseCase.swift
@@ -5,3 +5,20 @@
 //  Created by Antoneos Philip on 06/07/2026.
 //
 
+import Foundation
+
+protocol CompleteOrderUseCaseProtocol {
+    func execute(orderInput: OrderCreateBody) async throws -> CompleteOrderEntity
+}
+
+final class CompleteOrderUseCase: CompleteOrderUseCaseProtocol {
+    private let repository: CompleteOrderRepositoryProtocol
+
+    init(repository: CompleteOrderRepositoryProtocol) {
+        self.repository = repository
+    }
+
+    func execute(orderInput: OrderCreateBody) async throws -> CompleteOrderEntity {
+        return try await repository.placeOrder(orderInput: orderInput)
+    }
+}
